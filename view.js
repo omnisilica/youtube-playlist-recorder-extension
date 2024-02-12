ad81debd39900d2865d62af824850fc88421ae19
omnisilica <oxmaverix@gmail.com>
2024-02-12 01:21:34 -0500

    First Commit.

---
+++ view.js
@@ -0,0 +1,8 @@
+
+/*
+- Check whether user is in a youtube page
+- Check whether there's a playlist in the current page
+- Display the playlist on the current page
+- Present button to allow user to start recording the playlist
+- Everytime a new video loads that video will be added to the queue of videos that's being created, as long as the user is in a youtube page that's playing the same playlist they started recording
+*/
