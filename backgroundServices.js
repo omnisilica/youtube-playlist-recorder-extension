e4811357b8b73b7b500c82724f8bba2100fc45e2
omnisilica <oxmaverix@gmail.com>
2024-03-03 15:35:19 -0500

    Completed v1.0.0

--- backgroundServices.js
+++ backgroundServices.js
@@ -3,9 +3,37 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     console.log("Tab has been updated.");
 
-    let recordingOn = false;
 
-    const playListRecordings = {};
+    let recordingOn = false;
+    const currentVideoMessage = "current_video";
+
+    const playlistRecordings = {};
+
+    let plid = "123";
+    let pliddata = {currentVideo:"456"}
+    playlistRecordings.plid = pliddata
+
+    //console.log(`Example: ${playlistRecordings.plid.currentVideo}`);
+    
+    const getCurrentList = async (playlistID) => {
+        //console.log(`Result from async function: ${playlistID}`);
+        let result = await chrome.storage.local.get([playlistID]);
+
+        return result;
+        
+        /*let currentList;
+        console.log(`playlist id from getCurrentList: ${playlistID}`);
+
+        if(playlistID) {
+            currentList = chrome.storage.local.get([playlistID]).then((result) => {
+
+        });
+        }
+
+        console.log(currentList);
+        
+        return currentList;*/
+    }
     
     function turnRecordingOn(turnOn){
         
@@ -15,26 +43,177 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
 
         return recordingOn;
     }
-    
-    function handleDOM(domContent, sender, sendResponse) {
-        console.log("YouTube playlists from content script below.");
-        //console.log(domContent.youtubePlaylist);
-        if (domContent.messageType === "playlistDetails") {
-            console.log(domContent);
-            sendResponse({response: turnRecordingOn(true)});
-            sendResponse({response: "Thank you for your service."});
-        } else if(domContent.messageType === "currentRecordingStatus") {
-            sendResponse({response: turnRecordingOn(false)});
+
+    function deletPlaylist(playlistID) {
+        chrome.storage.local.remove(playlistID);
+    }
+
+    function updatePlaylist(playlistToUpdate){
+        let playlist = JSON.parse(playlistToUpdate);
+        let processedPlaylistVideos = Array.from(new Set(playlist.videos));
+        playlist.videos = processedPlaylistVideos;
+        const playlistToUpdateID = playlist.playlistID;
+
+        console.log(processedPlaylistVideos);
+        console.log(`Passed playlist: ${playlist.currentVideo}`);
+
+        chrome.storage.local.set({[playlistToUpdateID]: playlist}).then(() => {
+            console.log("Updated playlist");
+            console.log(playlist.videos);
+            //console.log(JSON.parse(playlistToAddStringified));
+        });
+
+        //deletPlaylist(playlist.playlistID);
+    }
+
+    function addVideoToPlaylist(video){
+        //get video and playlist parameter from view and if recording is on,
+        //search if the playlist exists in storag. If it does exist and it
+        //has the same tabid as the tabid from the video obtained from the
+        //view, add it to the playlist that it's a part of.
+
+
+        const videoPlaylistID = video.playlistID;
+        var playlistToUpdate;
+        var playlistVideos;
+        var playlistToSend;
+
+        chrome.storage.local.get(null).then((result) => {
+            console.log(result);
+            console.log(`Playlist ID: ${videoPlaylistID}`);
+            let associatedPlaylist = result[videoPlaylistID];
+            let associatedPlaylistVideos = associatedPlaylist.videos;
+            let videoToAdd = video.currentVideo;
+            console.log(`Associated playlist videos: ${associatedPlaylist}`);
+            if(associatedPlaylist && !(associatedPlaylistVideos.videoToAdd)) {
+                playlistToUpdate = result[videoPlaylistID];
+                playlistVideos = playlistToUpdate.videos;
+                console.log(`Current Video To Be Added: ${video.currentVideo}`)
+                playlistVideos.push(video.currentVideo);
+                console.log(`Playlist as it stands: ${playlistVideos}`);
+                playlistToUpdate.videos = playlistVideos;
+                playlistToSend = JSON.stringify(playlistToUpdate);
+                console.log(`New playslist: ${playlistToUpdate}`);
+                updatePlaylist(playlistToSend);
+            }
+        })
+
+        //console.log(`New playslist: ${playlistToSend}`);
+        
+        /*chrome.storage.local.get([videoPlaylistID]).then((result) => {
+
+            console.log(`Playlist exists: ${result}`);
+        });*/
+
+        /*if(playlistRecordings.videoPlaylistID) {
+            console.log(`Exists`);
+        }*/
+    }
+
+    function addPlaylist(playlist) {
+        let playlistInStorage = false;
+        
+        const playlistToAddID = playlist.playlistID;
+        console.log(playlistToAddID);
+        console.log(`Add playlist current video: ${playlist.currentVideo}`);
+
+        chrome.storage.local.get(null).then((result) => {
+            console.log(result);
+            
+            if(result.playlistToAddID) {
+                playlistInStorage = true;
+            }
+            
+            console.log(`Value from storage: ${playlistInStorage}, ${playlistToAddID}`)
+        })
+
+        if(!playlistInStorage) {
+            let playlistToAdd = {};
+            delete playlist.messageType;
+
+            playlist.videos.push(playlist.currentVideo);
+
+            //playlistToAdd[playlistToAddID] = playlist;
+
+            //playlistToAddStringified = JSON.stringify(playlistToAdd);
+
+            
+            
+            chrome.storage.local.set({[playlistToAddID]: playlist}).then(() => {
+                console.log("Added playlist");
+                //console.log(JSON.parse(playlistToAddStringified));
+            });
+            
+            //console.log(`Playlist ID: ${playlistRecordings.playlistToAddID}`);
+        } else {
+            console.log("Playlist already stored.");
+            
+            //addVideoToPlaylist(playlist);
         }
+
+        chrome.storage.local.get([playlistToAddID]).then((result) => {
+            let resultStringified = JSON.stringify(result);
+            console.log(`Playlist exists: ${resultStringified}`);
+        });
+        
+    }
+    
+    function handleRequest(request, sender, sendResponse) {
+        console.log("YouTube playlists from content script below.");
+        //console.log(request.youtubePlaylist);
+        let recordingStatus;
+        if (request.messageType === "playlistDetails") {
+            console.log(`Playlist details current video: ${request.currentVideo}`);
+            recordingStatus = turnRecordingOn(true);
+
+            
+            addPlaylist(request);
+            sendResponse({response: recordingStatus});
+            sendResponse({response: "Thank you for your service."});
+        } else if(request.messageType === "currentRecordingStatus") {
+            recordingStatus = turnRecordingOn(false);
+            sendResponse({response: recordingStatus});
+        } else if(request.messageType === "get_current_list"){
+            //console.log("trying to add playlist");
+            sendResponse({response: getCurrentList(request.playlist)});
+        }/*else if(request.messageType === "video_to_add_details") {
+            //console.log(request.currentVideo);
+            addVideoToPlaylist(request);
+            sendResponse({response: "Thank you for your service."});
+        }*/
         
         
+    }
+
+    function handleError(error) {
+        console.log(error);
     }
 
     function handlePlaylistToRecord(message) {
         console.log(message);
     }
 
-    chrome.runtime.onMessage.addListener(handleDOM);
+    chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    }, ([tab]) => {
+        console.log(`Current Tab URL=> ${tab.url}`);
+        let video = {};
+        let tabParameters = tab.url.split("?")[1];
+        let ytURLParameters = new URLSearchParams(tabParameters);
+        video["currentVideo"] = ytURLParameters.get("v");
+        video["playlistID"] = ytURLParameters.get("list");
+        console.log(`Current Video Details - ${video.currentVideo}`);
+        console.log(`Current Video Details - ${video.playlistID}`);
+
+        if(video.playlistID){
+            addVideoToPlaylist(video);
+        }
+    });
+
+    chrome.runtime.onMessage.addListener(handleRequest);
+
+   
 
     //Check to see if a tab exists in the first place and if that tab existent tab has a youtube video on it.
     if (tab.url && tab.url.includes("youtube.com/watch")) {
--- view.js
+++ view.js
@@ -20,8 +20,115 @@ document.addEventListener("DOMContentLoaded", async () => {
     const currentTabParameters = currentTab.url.split("?")[1];
     const ytURLParameters = new URLSearchParams(currentTabParameters);
     const ytListParameter = ytURLParameters.get("list");
+    const ytVideoParameter = ytURLParameters.get("v");
     //currentTab.playlistRecording = false;
     const status = {recordingInProgress:false};
+
+    console.log(`Current Video: ${ytVideoParameter}`);
+
+    chrome.runtime.onMessage.addListener((request) => {
+      console.log(`Message from background script: ${request}`);
+    });
+
+    
+    async function downloadCurrentList(playlist) {
+      let listOfVidoesFile;
+      let videosToDownload = playlist.videos;
+      let stopRecordingButton = document.querySelector(".stop-recording-button");
+      let playlistNames = [];
+      let videoTitle;
+      stopRecordingButton.disabled = true;
+
+      for(let i = 0; i < videosToDownload.length; i++){
+        let currentVideoID = videosToDownload[i];
+        const response = await
+
+        fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${currentVideoID}&key=${api_key}`)
+         
+        const videoSnippetResponse = await response.json();
+
+        videoTitle = videoSnippetResponse.items[0].snippet.title;
+        playlistNames.push(videoTitle);
+        playlistNames.push("\n");
+      }
+
+      console.log("Downloading");
+
+      let generateTextFile = (text) => {
+        let listData = new Blob([text], {type: "text/plain"});
+
+        if(listOfVidoesFile !== null) {
+          window.URL.revokeObjectURL(listOfVidoesFile);
+        }
+
+        listOfVidoesFile = window.URL.createObjectURL(listData);
+
+        return listOfVidoesFile;
+      }
+
+        let downloadLink = document.querySelector(".download-link");
+        downloadLink.href = generateTextFile(playlistNames);
+        downloadLink.style.display = "block";
+    }
+
+    async function displayCurrentList(playlist){
+      let videosToDisplay = playlist.videos;
+      let root_ = document.querySelector(".root_");
+      let playlistTitle = document.querySelector(".playlist-title").getElementsByTagName("p")[0];
+      playlistTitle = playlistTitle.innerText;
+      let listCurrentPlaylistVideosDiv = document.createElement("div");
+      
+      let currentPlaylistDiv = document.createElement("div");
+      let viewAllButton = document.querySelector(".view-all-button");
+      currentPlaylistDiv.classList.add("current-playlist");
+      viewAllButton.disabled = true;
+      
+      
+      
+      listCurrentPlaylistVideosDiv.classList.add("list-videos");
+      //root_.appendChild(currentPlaylistDiv);
+      console.log(`Trying to display current list: ${playlist.videos}`);
+      console.log(playlistTitle);
+
+      for(let i = 0; i < videosToDisplay.length; i++) {
+        let currentVideoID = videosToDisplay[i];
+        let videoTitle;
+        let videoThumbnail;
+        let videoItem = document.createElement("div");
+        let currentVideoItemImg = document.createElement("img");
+        let currentVideoTitleParagraph = document.createElement("p");
+        
+        currentVideoItemImg.classList.add("current-video-item-img");
+        currentVideoTitleParagraph.classList.add("current-video-title");
+        videoItem.classList.add("video-item-container");
+        console.log(currentVideoID);
+
+        const response = await
+
+        //fetch(`https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${currentVideoID}&key=${api_key}`);
+        fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${currentVideoID}&key=${api_key}`)
+         
+        const videoSnippetResponse = await response.json();
+
+        videoTitle = videoSnippetResponse.items[0].snippet.title;
+        videoThumbnail = videoSnippetResponse.items[0].snippet.thumbnails.default.url;
+        console.log(`Title: ${videoTitle}`);
+        console.log(`Thumbnail" ${videoThumbnail}`);
+
+        currentVideoItemImg.src = videoThumbnail;
+        currentVideoItemImg.alt = `Thumbnail image for ${videoTitle}`;
+        currentVideoItemImg.title = `Thumbnail image for ${videoTitle}`;
+        currentVideoTitleParagraph.innerText = videoTitle;
+        
+        videoItem.appendChild(currentVideoItemImg);
+        videoItem.appendChild(currentVideoTitleParagraph);
+        currentPlaylistDiv.appendChild(videoItem);
+
+        
+
+      }
+      root_.appendChild(currentPlaylistDiv);
+    }
 
     function getRecordingStatus(){
       const request = {
@@ -35,10 +142,13 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     function recordingInProgressUI() {
       const playlistDetailsSectionDiv = document.querySelector(".record-playlist");
+      playlistDetailsSectionDiv.classList.add("recording-options");
       const viewAllButton = document.createElement("button");
+      viewAllButton.classList.add("view-all-button");
       viewAllButton.innerHTML = "View All";
 
       const stopRecordingButton = document.createElement("button");
+      stopRecordingButton.classList.add("stop-recording-button");
       stopRecordingButton.innerHTML = "Stop Recording";
 
       playlistDetailsSectionDiv.replaceChildren(viewAllButton, stopRecordingButton);
@@ -50,15 +160,51 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       if(status.recordingInProgress) {
         recordingInProgressUI();
+        //addVideoToPlaylist();
       }
-      
       console.log(`After change: ${status.recordingInProgress}`);
+
+      let viewAllButton = document.querySelector(".view-all-button");
+      let stopRecordingButton = document.querySelector(".stop-recording-button");
+
+       console.log(viewAllButton);
+       if(viewAllButton){
+          viewAllButton.addEventListener("click", (event) => {
+            let request = {
+              messageType: "get_current_list",
+              playlist: ytListParameter
+            };
+            console.log("click works");
+
+            chrome.storage.local.get(null).then((result) => {
+              console.log(`List called from view: ${JSON.stringify(result[ytListParameter])}`);
+              displayCurrentList(result[ytListParameter]);
+            });
+
+            //chrome.runtime.sendMessage(request).then(displayCurrentList, handleError);
+        });
+       }
+
+       if(stopRecordingButton) {
+        console.log(stopRecordingButton);
+        stopRecordingButton.addEventListener("click", (event) => {
+
+          chrome.storage.local.get(null).then((result) => {
+            downloadCurrentList(result[ytListParameter]);
+          });
+
+      });
+       }
     }
     
     function handleResponse(message){
       //console.log(`Message from the background script: ${message.response}`);
       status.recordingInProgress = message.response;
       console.log(`Recording Status: ${status.recordingInProgress}`);
+      
+      //console.log("event below");
+      //console.log(event);
+      
   }
 
   function handleError(error){
@@ -67,11 +213,24 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     function recordPlaylist(event, playlistToRecord) {
       console.log(currentTab);
-      
       //recordingInProgressUI();
 
       
       chrome.runtime.sendMessage(playlistToRecord).then(handleResponse, handleError);
+    }
+
+    function addVideoToPlaylist() {
+
+      const videoToAdd = {
+        messageType: "video_to_add_details",
+        playlistID: ytListParameter,
+        currentVideo: ytVideoParameter,
+        tabID: currentTab.id
+      }
+
+      console.log("Adding video");
+
+      chrome.runtime.sendMessage(videoToAdd).then(handleResponse, handleError);
     }
 
     
@@ -80,16 +239,16 @@ document.addEventListener("DOMContentLoaded", async () => {
     if(currentTab.url.includes("youtube.com/watch") && ytListParameter) {
         console.log("User is on a page with an active playlist.");
         
-        const response = await
+        //const response = await
 
          //fetch("https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=PLW-N9qZtco2U7hjPBkakwc4fdQ9v095ey&key=AIzaSyA_r7zJsZa4Wv5DAjd552eOPTfU8heneAI");
-         fetch(`https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${ytListParameter}&key=${api_key}`);
-         const playlistSnippetResponse = await response.json();
+         //fetch(`https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${ytListParameter}&key=${api_key}`);
+         //const playlistSnippetResponse = await response.json();
          
-         const defaultPlaylistThumbnail = playlistSnippetResponse.items[0].snippet.thumbnails.default.url;
-         const playlistTitle = playlistSnippetResponse.items[0].snippet.title;
+         //const defaultPlaylistThumbnail = playlistSnippetResponse.items[0].snippet.thumbnails.default.url;
+         //const playlistTitle = playlistSnippetResponse.items[0].snippet.title;
          
-         console.log(`YouTube Default Thumbanil URL: ${defaultPlaylistThumbnail}`);
+         //console.log(`YouTube Default Thumbanil URL: ${defaultPlaylistThumbnail}`);
         
        const tempImg = "https://i.ytimg.com/vi/2zEiiZi2DKk/default.jpg";
        const tempTitle = "Worship";
@@ -98,6 +257,7 @@ document.addEventListener("DOMContentLoaded", async () => {
        const playlistDetailsSectionImg = document.createElement("img");
        const playlistDetailsSectionTitleDiv = document.createElement("div");
        const playlistDetailsSectionTitleParagraph = document.createElement("p");
+       const recordButton = document.createElement("button");
        let playlistDetailsSectionRecordOptionDiv;
        let playlistDetailsSectionRecordParagraph;
        
@@ -107,12 +267,12 @@ document.addEventListener("DOMContentLoaded", async () => {
        playlistDetailsSectionTitleDiv.className = "playlist-title";
        
 
-       playlistDetailsSectionImg.src = defaultPlaylistThumbnail;
-       //playlistDetailsSectionImg.src = tempImg;
-       playlistDetailsSectionImg.alt = `Thumbnail image for ${playlistTitle}`;
-       playlistDetailsSectionImg.title = `Thumbnail image for ${playlistTitle}`;
-       playlistDetailsSectionTitleParagraph.innerHTML = playlistTitle;
-      // playlistDetailsSectionTitleParagraph.innerHTML = tempTitle;
+       //playlistDetailsSectionImg.src = defaultPlaylistThumbnail;
+       playlistDetailsSectionImg.src = tempImg;
+      //  playlistDetailsSectionImg.alt = `Thumbnail image for ${playlistTitle}`;
+      //  playlistDetailsSectionImg.title = `Thumbnail image for ${playlistTitle}`;
+      //  playlistDetailsSectionTitleParagraph.innerHTML = playlistTitle;
+      playlistDetailsSectionTitleParagraph.innerHTML = tempTitle;
 
       
       
@@ -122,10 +282,13 @@ document.addEventListener("DOMContentLoaded", async () => {
         playlistDetailsSectionRecordParagraph = document.createElement("p");
        playlistDetailsSectionRecordOptionDiv.className = "record-playlist";
         playlistDetailsSectionRecordParagraph.innerHTML = "Record";
-        playlistDetailsSectionRecordOptionDiv.appendChild(playlistDetailsSectionRecordParagraph);
+        
+        recordButton.classList.add("record-button");
+        recordButton.appendChild(playlistDetailsSectionRecordParagraph);
+        playlistDetailsSectionRecordOptionDiv.appendChild(recordButton);
        
         
-
+      
        playlistDetailsSectionTitleDiv.appendChild(playlistDetailsSectionTitleParagraph);
        
 
@@ -140,34 +303,27 @@ document.addEventListener("DOMContentLoaded", async () => {
         playlistID: ytListParameter,
         playlistTitle: tempTitle,
         playlistDefaultThumbnail: tempImg,
-        tabID: currentTab.id
+        tabID: currentTab.id,
+        currentVideo: ytVideoParameter,
+        videos: []
       }
       
       getRecordingStatus();
 
-       playlistDetailsSectionRecordOptionDiv.addEventListener("click", (event) => {
+      
+      recordButton.addEventListener("click", (event) => {
         //currentTab.playlistRecording = true;
         //console.log(recordingInProgress);
         recordPlaylist(event, playlistToRecord);
        });
+
+       
+       
+
+       
         
 
-        /*
-          Features:
-          - Allow multiple playlist recordings
-            1. Add event listener to record button to start recording playlist using playlist id.
-               Summary: Basically, add a the mechanism to record and the page to view the recordings so far. Then add the option to take notes and have that note attached to the video that was playing at the time.
-            2. Change record button to stop recording button.
-              - Add event listener to stop recording this playlist when clicked.
-              - Add mechanism to stop listening if tab is closed
-            3. Add button next to stop recording button that will allow user to view what the recordings for a particular playlist
-          - Add reasons and instructions for why playlist isn't seen
-              - If there's a plyalist parameter but it's not being read, notify user and let them know how to make playlist unlisted or public              
-          - Allow user to add notes
-          - If user copies url of playlist and places it in a different tab then ask the user if they want to continue from where they left off or start fresh.
-          - When tab is closed without stop recording button clicked save the recording for a couple days.
-            - Create an option for a user to see the past recordings for the past couple days.
-          - Display how many videos in video cards*/
+        
 
     } else if (currentTab.url.includes("youtube.com")) {
         console.log("User is on a YouTube page, but not one with an active playlist on it.");
@@ -254,4 +410,53 @@ const render = (html, replacePresentHTML) => {
   ]
 }
 
+*/
+
+/*
+          Features:
+          - Allow multiple playlist recordings
+            1. Add event listener to record button to start recording playlist using playlist id.
+               Summary: Basically, add a the mechanism to record and the page to view the recordings so far. Then add the option to take notes and have that note attached to the video that was playing at the time.
+            2. Change record button to stop recording button.
+              - Add event listener to stop recording this playlist when clicked.
+              - Add mechanism to stop listening if tab is closed
+            3. Add button next to stop recording button that will allow user to view what the recordings for a particular playlist
+          - Add reasons and instructions for why playlist isn't seen
+              - If there's a plyalist parameter but it's not being read, notify user and let them know how to make playlist unlisted or public              
+          - Allow user to add notes
+          - If user copies url of playlist and places it in a different tab then ask the user if they want to continue from where they left off or start fresh.
+          - When tab is closed without stop recording button clicked save the recording for a couple days.
+            - Create an option for a user to see the past recordings for the past couple days.
+          - Display how many videos in video cards*/
+
+
+/*Up Next
+- We already have the video parameter. Create a function to take the
+  video id if the status.recordingInProgress value is true.
+- Within that function send a message to background script with the video
+  details and the associated playlist details.
+- Have background script add an object to the playlist recordings object.
+  If the playlist doesn't exist create a new one and add the video to the
+  playlist. Otherwise, simply add the video.
+- When user hits stop recording, change stop recording button to download
+  button. Later, figure out how to download a file in the browser.
+- When user hits view all change view add a list of the videos to the bottom
+  of the current card. Change the view all text to hide.
+- When user hits hide take away view that displays the list of videos.
+*/
+
+/*
+Considerations for future versions:
+- only record a playlist of a certain size due to byte limitations
+- establish an open connection
+- have things execute on tab update in background script, not view domcontentload
+- sepearate messages from actual object
+*/
+
+/*
+JavaScript concepts to solidify:
+- asynchronous javascript
+- stringify and parse objects in asynchronous fashion (if possible)
+- arrange functions, messages, event listeners, variables, and immediately executable code
+- how to manage state for chrome extensions
 */
